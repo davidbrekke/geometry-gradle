c3b1a07539e39fa9be92a835b9217687032b408c
David Magnuson <52757864+davidbrekke@users.noreply.github.com>
2021-10-29 08:38:20 -0500

    initial commit

---
+++ app/src/main/java/geometry/gradle/Sphere.java
@@ -0,0 +1,10 @@
+package geometry.gradle;
+
+public class Sphere {
+  public float volume(int radius){
+      return (float) ( Math.PI * ( radius * radius * radius ) * ( 4 / 3 ));
+  }
+  public float surfaceArea(int radius){
+      return (float) ( 4 * Math.PI * ( radius * radius ));
+  }
+}
